c08de82470a2682266b528d1cde4d786be4ac0a2
Yonela <yoza.futho@gmail.com>
2021-07-26 13:30:20 +0200

    reusable components setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components"
+import { CssBaseline } from "@material-ui/core";
+import { StylesProvider } from '@material-ui/core/styles';
 
-export const App = () => {
-  return <div>123</div>
-}
-export default App;
+const Global = createGlobalStyle`
+overflow-x: hidden;
+overflow-y: scroll;
+`;
+
+
+export const App = (props) => {
+const { children } = props;
+return (
+<StylesProvider injectFirst>
+<CssBaseline />
+<Global />
+{children}
+</StylesProvider>
+);
+}; 
