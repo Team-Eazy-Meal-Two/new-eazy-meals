613c45ce123a162887f8536de1a077c99adfeec0
Yonela <yoza.futho@gmail.com>
2021-07-27 10:36:08 +0200

    Done with the Button,Text,Input and have started with the Layout component but not done yet

--- src/components/Layout/Layout.Demo.jsx
+++ src/components/Layout/Layout.Demo.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import { Layout } from "./Layout";
- 
+
 export const Demo = () => {
-    return <Layout />
-}
+  return (
+    <div>
+      <Layout title="Hello World">Hello World</Layout>
+
+      <Layout title="Hello World" primary={["primary", "a"]}>
+        Hello World
+      </Layout>
+
+      <Layout
+        title="Hello World"
+        primary={["primary", "a"]}
+        secondary={["secondary", "a"]}
+      >
+        Hello World
+      </Layout>
+
+      <Layout
+        title="Hello World"
+        primary={["primary", "a"]}
+        secondary={["secondary", "a"]}
+        extra={["extra", "a"]}
+      >
+        Hello World
+      </Layout>
+    </div>
+  );
+};
 export default Demo;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import { tokens } from "../../data/tokens";
  
-export const Layout = () => {
+/**
+ * 
+ * @typedef {object}
+ * @property {string} lable
+ * @property {action}
+ */
+/**
+ * @typedef {object} props
+ * @property {JSX.Element} children
+ * @property {string} title
+ * @property {boolean} inverse
+ * @property {[string, string | function]} [primary]
+ * @property {[string, string | function]} [secondary]
+ * @property {[string, string | function]} [extra]
+ */
+
+/**
+ * @param {props} props 
+ * @returns {JSX.Element}
+ */
+export const Layout = (props) => {
+    const {} = props;
     return <div>123</div>
 }
 export default Layout;
