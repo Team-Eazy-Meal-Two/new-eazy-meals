a2f644b0b6d3ea170fd7662f33920d9f14f067b8
Busiswa <onakomini@gmail.com>
2021-07-27 11:05:08 +0200

    create new account start up

--- src/views/NewAccount.jsx
+++ src/views/NewAccount.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import {Layout} from '../components/Layout/Layout'
+import{ input} from '../components/Input/Input'
+import { tokens} from '../../src/data/tokens/tokens'
+import {useNewAccount}from './NewAcount.useNewAccount'
+
 
 export const NewAccount =()=>{
+    const { email,password,confirmPassword,setEmail,setPassword,setconfirmPassword}=useNewAccount()
     return (
         <Layout
         title='New Acount'
