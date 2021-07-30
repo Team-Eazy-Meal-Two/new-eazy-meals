dd684d6a26ef0202cba2bcbac705081ea7cd3ef4
Yonela <yoza.futho@gmail.com>
2021-07-30 12:23:03 +0200

    changes

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import {Layout} from "../../components/Layout"
+import { Layout } from "../../components/Layout";
 
 export const LandingPage = () => {
-  return <Layout inverse title="Eazy Meals" secondary={["Sign In","/auth/signin"]} 
-  primary={["Create Account","/auth/register"]}
-  ></Layout>
+  return (
+    <Layout
+      inverse
+      title="Eazy Meals"
+      secondary={["Sign In", "/auth/signin"]}
+      primary={["Create Account", "/auth/register"]}
+    ></Layout>
+  );
 };
 
 export default LandingPage;
